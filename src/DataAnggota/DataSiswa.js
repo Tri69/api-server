a56bdbb1f0c8274adafd224d2760f9ab4f876bca
Ilham Trihandoyo <114585143+Tri69@users.noreply.github.com>
2024-11-01 18:46:50 +0700

    Add files via upload

---
+++ src/DataAnggota/DataSiswa.js
@@ -0,0 +1,79 @@
+econst DataSiswaProgramming = [
+    {
+        id:1,
+        name:'Pasha Mulyanto Utomo',
+        class:'X TSM C',
+        nis: 9774
+    },{
+        id:2,
+        name: 'Dinda Saputri',
+        class:'XI TJKT A',
+        nis: 9252
+    },{
+        id:3,
+        name: 'Isnaeni Maylinda Werdianingrum ',
+        class:'X DKV C',
+        nis: 9980
+    },{
+        id:4,
+        name: 'Yuniata Muzaemi',
+        class:'X DKV C',
+        nis: 9998
+    },{
+        id:5,
+        name: 'Akhmad Muta`alli Wahab ',
+        class:'X TP C',
+        nis:9533
+    },{
+        id:6,
+        name: 'Ahmad Rokhmi Kamaluddin',
+        class:'X TP X',
+        nis:9531
+    },{
+        id:7,
+        name: 'Oby Wiko Wijaya ',
+        class:'X TP C',
+        nis: 9557
+    },{
+        id:8,
+        name: 'Bima Adesca Camara ',
+        class:'X DKV B',
+        nis:9935
+    },{
+        id:9,
+        name: 'Bhima Dharma Sutajaya',
+        class:'X TP B',
+        nis:9502
+    },{
+        id:10,
+        name: 'Muhammad Widwan Mulyawan',
+        class:'X TP B',
+        nis:9518
+    },{
+        id:11,
+        name: 'Anggoro Ndaru Bekti',
+        class:'X TJKT C',
+        nis: 9859
+    },{
+        id:12,
+        name: 'Ilham Tias Arystiono',
+        class:'XI TKRO C',
+        nis: 9112
+    },{
+        id:13,
+        name: 'Chaira Pika Vallyta',
+        class:'XII DKV C',
+        nis: 8889
+    },{
+        id:14,
+        name: 'Meylani Endah Palupi',
+        class:'XII DKV C',
+        nis:8904
+    },{
+        id:15,
+        name: 'Lukman Arrahman',
+        class:'XI TKR C',
+        nis:9115
+    }
+        
+];
